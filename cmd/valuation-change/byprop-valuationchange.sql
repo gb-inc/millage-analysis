ac23cbc49d274c64acd403965b7a96830e950e54
Samuel Vierling <svierling@keystone.edu>
2023-05-09 11:48:02 -0400

    query changes

---
+++ cmd/valuation-change/byprop-valuationchange.sql
@@ -0,0 +1,51 @@
+CREATE OR ALTER VIEW byprop_valuationchange AS
+    SELECT
+     P.TownShipBorough
+    ,IIF(TownshipOrBorough = 'B', TBC.[Description] + ' BORO', TBC.[Description] + ' TWP') DisName
+    ,P.ControlNumber
+    ,AN.ImprovementAssessment AS NewImprAssmt
+    ,AN.LandAssessment AS NewLandAssmt
+    ,ANOld.ImprovementAssessment AS OldImprAssmt
+    ,ANOld.LandAssessment AS OldLandAssmt
+    ,(AN.ImprovementAssessment - ANOLD.ImprovementAssessment) ImprDiff
+    ,(AN.LandAssessment - ANOLD.LandAssessment) LandDiff
+    FROM AssmtNew AN
+    INNER JOIN Property P
+        ON P.PropertyID = AN.PropertyID
+    INNER JOIN TownshipBoroughCodes TBC
+        ON P.TownShipBorough = tbc.TownshipID
+    INNER JOIN LandUseCode LUC
+        ON P.LandUse = LUC.Code
+    LEFT JOIN (
+        SELECT *
+        FROM (
+            SELECT *, RANK() OVER (PARTITION BY PropertyID ORDER BY AssessmentID DESC) AS rank
+            FROM AssmtNew
+            WHERE EffectiveDate = '2023-01-01 00:00:00.000'
+        ) ranked
+        WHERE rank = 1
+    ) ANOLD ON P.PropertyID = ANOLD.PropertyID
+    WHERE AN.CurrentAssessment = -1
+    AND AN.ApprovalDate > '2023-01-01 00:00:00.000'
+    AND P.ParcelEliminated IS NULL
+    AND P.LandUse NOT IN ('166','W10')
+    AND TBC.TownshipOrBorough IN ('T','B')
+    AND NOT luc.ParcelStatus = 'N'
+    /* Wayne Highlands School District Specific */
+    AND P.TownShipBorough IN
+    ('030',
+     '070',
+     '150',
+     '200',
+     '230',
+     '020',
+     '090',
+     '110',
+     '130',
+     '010',
+     '050',
+     '170',
+     '210',
+     '270') -- 273 in Wallenpaupack
+    /* Wayne Highlands School District Specific */
+    AND (AN.LandAssessment + AN.ImprovementAssessment) - (ANOLD.LandAssessment + ANOLD.ImprovementAssessment) >= 0
--- cmd/valuation-change/valuationchange.sql
+++ cmd/valuation-change/valuationchange.sql
@@ -1,41 +1,12 @@
 SELECT
- P.TownShipBorough
-,SUM(ANOld.LandAssessment) AS OldLandAssmt
-,SUM(ANOld.ImprovementAssessment) AS OldImprAssmt
-,SUM(AN.LandAssessment) AS NewLandAssmt
-,SUM(AN.ImprovementAssessment) AS NewImprAssmt
-,SUM((AN.LandAssessment - COALESCE(ANOLD.LandAssessment, 0.00))) LandDiff
-,SUM((AN.ImprovementAssessment - COALESCE(ANOLD.ImprovementAssessment, 0.00))) ImprDiff
-FROM AssmtNew AN
-INNER JOIN Property P
-    ON P.PropertyID = AN.PropertyID
-INNER JOIN TownshipBoroughCodes TBC
-    ON P.TownShipBorough = tbc.TownshipID
-INNER JOIN LandUseCode LUC
-    ON P.LandUse = LUC.Code
-LEFT JOIN (SELECT * FROM AssmtNew WHERE EffectiveDate <= '2023-01-01 00:00:00.000' AND CurrentAssessment = -1) ANOLD
-    ON P.PropertyID = ANOld.PropertyID
-WHERE AN.CurrentAssessment = -1
-AND P.ParcelEliminated IS NULL
-AND P.LandUse NOT IN ('166','W10')
-AND TBC.TownshipOrBorough IN ('T','B')
-AND NOT luc.ParcelStatus = 'N'
-/* Wayne Highlands School District Specific */
-AND P.TownShipBorough IN
-('030',
- '070',
- '150',
- '200',
- '230',
- '020',
- '090',
- '110',
- '130',
- '010',
- '050',
- '170',
- '210',
- '270') -- 273 in Wallenpaupack
-/* Wayne Highlands School District Specific */
-GROUP BY TownShipBorough
+ bpvc.TownShipBorough
+,bpvc.DisName
+,SUM(bpvc.NewImprAssmt) NewImprAssmt
+,SUM(bpvc.NewLandAssmt) NewLandAssmt
+,SUM(bpvc.OldImprAssmt) OldImprAssmt
+,SUM(bpvc.OldLandAssmt) OldLandAssmt
+,SUM(bpvc.ImprDiff) ImprDiff
+,SUM(bpvc.LandDiff) LandDiff
+FROM byprop_valuationchange bpvc
+GROUP BY TownShipBorough, DisName
 ORDER BY TownShipBorough
