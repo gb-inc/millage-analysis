5e3f78b6220bec29e670527295cec89d08ec9176
Samuel Vierling <svierling@keystone.edu>
2023-05-10 09:50:35 -0400

    update queries

--- cmd/assmt-increase/bytwp-assmtincrease.sql
+++ cmd/assmt-increase/bytwp-assmtincrease.sql
@@ -2,11 +2,11 @@ SELECT
  bpic.TownShipBorough
 ,bpic.DisName
 ,SUM(bpic.NewImprAssmt) NewImprAssmt
-,SUM(bpic.NewLandAssmt) NewLandAssmt
+--,SUM(bpic.NewLandAssmt) NewLandAssmt
 ,SUM(bpic.OldImprAssmt) OldImprAssmt
-,SUM(bpic.OldLandAssmt) OldLandAssmt
+--,SUM(bpic.OldLandAssmt) OldLandAssmt
 ,SUM(bpic.ImprDiff) ImprDiff
-,SUM(bpic.LandDiff) LandDiff
+--,SUM(bpic.LandDiff) LandDiff
 FROM byprop_assmtincrease bpic
 GROUP BY TownShipBorough, DisName
 ORDER BY TownShipBorough
