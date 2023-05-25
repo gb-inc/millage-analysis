5f5cee986306d28ecfff7eb4cb741250e2810d86
orklepee <svierling@keystone.edu>
2023-05-25 17:16:26 -0400

    udpates for July 7th stuff

--- cmd/assmt-increase/sql/byprop-assmtincrease.sql
+++ cmd/assmt-increase/sql/byprop-assmtincrease.sql
@@ -1,37 +1,63 @@
+-- View operating off temporary table storing formal notice values
 CREATE OR ALTER VIEW byprop_assmtincrease AS
-    SELECT
-     P.TownShipBorough
+    SELECT 
+    P.TownShipBorough
     ,IIF(TownshipOrBorough = 'B', TBC.[Description] + ' BORO', TBC.[Description] + ' TWP') DisName
     ,P.ControlNumber
     ,AN.ImprovementAssessment AS NewImprAssmt
-    ,AN.LandAssessment AS NewLandAssmt
-    ,ANOld.ImprovementAssessment AS OldImprAssmt
-    ,ANOld.LandAssessment AS OldLandAssmt
-    ,(AN.ImprovementAssessment - ANOLD.ImprovementAssessment) ImprDiff
-    ,(AN.LandAssessment - ANOLD.LandAssessment) LandDiff
-    FROM AssmtNew AN
-    INNER JOIN Property P
-        ON P.PropertyID = AN.PropertyID
+    ,NoticeVals.IMPR AS OldImprAssmt
+    ,(AN.ImprovementAssessment - NoticeVals.IMPR) ImprDiff
+    FROM Sheet1$ NoticeVals
+    LEFT JOIN Property P
+        ON P.ControlNumber = NoticeVals.[CONTROL]
+    INNER JOIN AssmtNew AN
+        ON AN.PropertyID = P.PropertyID
     INNER JOIN TownshipBoroughCodes TBC
         ON P.TownShipBorough = tbc.TownshipID
     INNER JOIN LandUseCode LUC
         ON P.LandUse = LUC.Code
-    LEFT JOIN (
-        SELECT *
-        FROM (
-            SELECT *, RANK() OVER (PARTITION BY PropertyID ORDER BY AssessmentID DESC) AS rank
-            FROM AssmtNew
-            WHERE EffectiveDate >= '2022-07-01 00:00:00.000'
-            AND EffectiveDate <= '2023-01-01 00:00:00.000'
-            AND ApprovalDate >= '2022-02-22 00:00:00.000' --oldest approval date for 07/01 effective dates
-		    AND ApprovalDate < '2023-01-01 00:00:00.000'
-        ) ranked
-        WHERE rank = 1
-    ) ANOLD ON P.PropertyID = ANOLD.PropertyID
-    WHERE AN.CurrentAssessment = -1
-    AND AN.ApprovalDate > '2022-07-01 00:00:00.000'
-    AND P.ParcelEliminated IS NULL
+    WHERE P.ParcelEliminated IS NULL
+    AND AN.CurrentAssessment = -1
     AND P.LandUse NOT IN ('166','W10')
     AND TBC.TownshipOrBorough IN ('T','B')
     AND NOT luc.ParcelStatus = 'N'
-    AND (AN.ImprovementAssessment) - (ANOLD.ImprovementAssessment) >= 0
+    AND (AN.ImprovementAssessment) - (NoticeVals.IMPR) >= 0
+
+-- Old View
+-- CREATE OR ALTER VIEW byprop_assmtincrease AS
+--     SELECT
+--      P.TownShipBorough
+--     ,IIF(TownshipOrBorough = 'B', TBC.[Description] + ' BORO', TBC.[Description] + ' TWP') DisName
+--     ,P.ControlNumber
+--     ,AN.ImprovementAssessment AS NewImprAssmt
+--     ,AN.LandAssessment AS NewLandAssmt
+--     ,ANOld.ImprovementAssessment AS OldImprAssmt
+--     ,ANOld.LandAssessment AS OldLandAssmt
+--     ,(AN.ImprovementAssessment - ANOLD.ImprovementAssessment) ImprDiff
+--     ,(AN.LandAssessment - ANOLD.LandAssessment) LandDiff
+--     FROM AssmtNew AN
+--     INNER JOIN Property P
+--         ON P.PropertyID = AN.PropertyID
+--     INNER JOIN TownshipBoroughCodes TBC
+--         ON P.TownShipBorough = tbc.TownshipID
+--     INNER JOIN LandUseCode LUC
+--         ON P.LandUse = LUC.Code
+--     LEFT JOIN (
+--         SELECT *
+--         FROM (
+--             SELECT *, RANK() OVER (PARTITION BY PropertyID ORDER BY AssessmentID DESC) AS rank
+--             FROM AssmtNew
+--             WHERE EffectiveDate >= '2022-07-01 00:00:00.000'
+--             AND EffectiveDate <= '2023-01-01 00:00:00.000'
+--             AND ApprovalDate >= '2022-02-22 00:00:00.000' --oldest approval date for 07/01 effective dates
+-- 		    AND ApprovalDate < '2023-01-01 00:00:00.000'
+--         ) ranked
+--         WHERE rank = 1
+--     ) ANOLD ON P.PropertyID = ANOLD.PropertyID
+--     WHERE AN.CurrentAssessment = -1
+--     AND AN.ApprovalDate > '2022-07-01 00:00:00.000'
+--     AND P.ParcelEliminated IS NULL
+--     AND P.LandUse NOT IN ('166','W10')
+--     AND TBC.TownshipOrBorough IN ('T','B')
+--     AND NOT luc.ParcelStatus = 'N'
+--     AND (AN.ImprovementAssessment) - (ANOLD.ImprovementAssessment) >= 0
--- cmd/assmt-increase/sql/total-taxable.sql
+++ cmd/assmt-increase/sql/total-taxable.sql
@@ -6,8 +6,14 @@ INNER JOIN TownshipBoroughCodes tbc
 on P.TownShipBorough = tbc.TownshipID
 INNER JOIN LandUseCode luc
 ON P.LandUse = luc.Code
+INNER JOIN [Ownership] O
+ON P.PropertyID = O.PropertyID
+INNER JOIN [Owner]
+ON O.OwnershipID = [Owner].OwnershipID
 WHERE An.CurrentAssessment <> 0
 AND P.ParcelEliminated IS NULL
 AND P.LandUse NOT IN ('166', 'W10')
 AND tbc.TownshipOrBorough IN ('T','B')
 AND NOT luc.ParcelStatus = 'N'
+AND O.CurrentOwnership <> 0
+AND [Owner].ClientToReceiveTaxBill <> 0
